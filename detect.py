f7f85377dc4adb58580c5e59536a358317e212f3
Yothe <79477704+Yothe@users.noreply.github.com>
2021-04-06 09:05:45 +0200

    Update detect.py

--- detect.py
+++ detect.py
@@ -128,7 +128,7 @@ def detect(save_img=False):
                         Color, thin = [0,255,0], 1
                         cx, cy = (x2-x1)/2, (y2-y1)/2
                         for x in violations: 
-                          if abs(cx-x[0]) <=10 and abs(cy-x[1]) <=10: 
+                          if ((cx-x[0])**2 + (cy-x[1])**2)**(0.5) <98: #98 pixels
                             Color, thin = [0,0,255], 1
 
                         plot_one_box(xyxy, im0, label=label, color=Color, line_thickness=thin)
