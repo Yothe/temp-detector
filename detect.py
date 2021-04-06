1edc45fe91535064077d615822a11fe236188e21
Yothe <79477704+Yothe@users.noreply.github.com>
2021-04-06 09:32:25 +0200

    Update detect.py

--- detect.py
+++ detect.py
@@ -128,7 +128,8 @@ def detect(save_img=False):
                         Color, thin = [0,255,0], 1
                         cx, cy = (x2-x1)/2, (y2-y1)/2
                         for x in violations: 
-                          if ((cx-x[0])**2 + (cy-x[1])**2)**(0.5) < 98: #98 pixels = 1m
+                          dist = if ((cx-x[0])**2 + (cy-x[1])**2)**(0.5) 
+                          if dist < 98 and dist > 0: #98 pixels = 1m; dist=0 means centroid has found itself 
                             Color, thin = [0,0,255], 1
 
                         plot_one_box(xyxy, im0, color=Color, line_thickness=thin)
