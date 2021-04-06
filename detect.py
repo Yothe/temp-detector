55ee08dc4294de90a063967d42d28c58a3698406
Yothe <79477704+Yothe@users.noreply.github.com>
2021-04-06 10:16:51 +0200

    Update detect.py

--- detect.py
+++ detect.py
@@ -129,7 +129,7 @@ def detect(save_img=False):
                         for x in violations: 
                           dist = ((cx-x[0])**2 + (cy-x[1])**2)**(0.5) # distance between cnetriods
                           #print(dist)
-                          if dist < 10 and dist > 0: #98 pixels = 1m; dist=0 means centroid has found itself 
+                          if dist < 1 and dist > 0: #98 pixels = 1m; dist=0 means centroid has found itself 
                             Color = [0,0,255]
 
                         plot_one_box(xyxy, im0, color=Color, line_thickness=1)    ### modify bounding box
